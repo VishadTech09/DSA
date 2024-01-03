77d03910c2bb381ba1ca62c566ba8d0512d8ec41
Vishad Patel <127468872+VishadTech09@users.noreply.github.com>
2024-01-03 22:33:19 +0530

    Solve sql problem on 03-01-2024  leetcode

---
+++ Leetcode/Java/NumberReduceToZero.java
@@ -0,0 +1,20 @@
+/**
+ * Title : 1342. Number of Steps to Reduce a Number to Zero
+ * Given an integer num, return the number of steps to reduce it to zero. 
+ * In one step, if the current number is even, you have to divide it by 2, otherwise, you have to subtract 1 from it.
+ */
+public class NumberReduceToZero {
+    public int numberOfSteps(int num) {
+        int counter = 0;
+        while (num > 0) {
+            if (num % 2 == 0) {
+                num = num / 2;
+            } else {
+                num = num - 1;
+            }
+            counter++;
+        }
+
+        return counter;
+    }
+}
